4028179f1ccffbed561b4d1182c1f51a9a830b48
Dylan-JC <dylan.269890@nv.ccsd.net>
2023-01-05 11:31:36 -0800

    change

--- Problem_8.cpp
+++ Problem_8.cpp
@@ -5,9 +5,17 @@ using namespace std;
 
 int main()
 {
+
+		cout << "************************************\n"
+		cout << "********        Billy       ********\n"
+		cout << "********        Shape       ********\n"
+		cout << "********        Maker       ********\n"
+		cout << "************************************\n"
+/*
 	ofstream MyFile("test.txt");
 
 	MyFile << "File go brrrrrrrrrrrrrrrrr\n";
 
 	MyFile.close();
+	*/
 }
