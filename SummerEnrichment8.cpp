75506217940dc9a864eef8b6a5d7ceb402e29d33
Calvin Cheam <88858058+CCheam@users.noreply.github.com>
2023-01-09 11:38:51 -0800

    Update SummerEnrichment8.cpp

--- SummerEnrichment8.cpp
+++ SummerEnrichment8.cpp
@@ -5,15 +5,22 @@ using namespace std;
 
 int main()
 {
-       fstream inp_file;
+       //title screen
 	cout << "************************************\n";
 	cout << "********       Billy        ********\n";
 	cout << "********       Shape        ********\n";
 	cout << "********       Maker        ********\n";
 	cout << "************************************\n";
-
+	//variable setup
+	fstream inp_file;
+	char letter;
+	int size;
+	//input ask/collection
 	cout << "Please enter the input file: ";
 	cin >>inp_file;
+	cout<<"Please input the shape, letter, and size to encode";
+	cin>>
+	cin>>
 		/*
 			ofstream MyFile("test.txt");
 
