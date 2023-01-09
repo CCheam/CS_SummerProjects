04940ef2ec445488858af88762402024307daf0f
Calvin Cheam <88858058+CCheam@users.noreply.github.com>
2023-01-09 11:27:27 -0800

    Update SummerEnrichment8.cpp

--- SummerEnrichment8.cpp
+++ SummerEnrichment8.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-
+       fstream inp_file;
 	cout << "************************************\n";
 	cout << "********       Billy        ********\n";
 	cout << "********       Shape        ********\n";
@@ -13,6 +13,7 @@ int main()
 	cout << "************************************\n";
 
 	cout << "Please enter the input file: ";
+	cin >>inp_file;
 		/*
 			ofstream MyFile("test.txt");
 
